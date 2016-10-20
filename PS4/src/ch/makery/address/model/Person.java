67973c5c80e53e0a8c0f73a68fdfb570e6cef176
achouri <achouri@128.4.209.25>
2016-10-20 14:45:43 -0400

    Everything done up to part 5. do part 6 only

--- PS4/src/ch/makery/address/model/Person.java
+++ PS4/src/ch/makery/address/model/Person.java
@@ -2,6 +2,9 @@ package ch.makery.address.model;
 
 import java.time.LocalDate;
 
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import ch.makery.address.util.LocalDateAdapter;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleIntegerProperty;
@@ -106,9 +109,10 @@ public class Person {
     public StringProperty cityProperty() {
         return city;
     }
-
+    @XmlJavaTypeAdapter(LocalDateAdapter.class)
     public LocalDate getBirthday() {
-        return birthday.get();
+    	return birthday.get();
+        
     }
 
     public void setBirthday(LocalDate birthday) {
